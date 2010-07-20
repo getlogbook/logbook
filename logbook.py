45fd45417bfcf9f12edf19796506b4886a8cbfdf
mitsuhiko <none@none>
2010-07-20 17:53:39 +0100

    More cleanup

--- logbook.py
+++ logbook.py
@@ -9,7 +9,6 @@ from __future__ import with_statement
 import os
 import sys
 import time
-import cStringIO
 import traceback
 import warnings
 import thread
@@ -244,18 +243,14 @@ class Handler(object):
         return rv
 
     def emit(self, record):
-        """
-        Do whatever it takes to actually log the specified logging record.
+        """Do whatever it takes to actually log the specified logging record.
 
         This version is intended to be implemented by subclasses and so
         raises a NotImplementedError.
         """
-        raise NotImplementedError('emit must be implemented '
-                                  'by Handler subclasses')
 
     def flush(self):
-        """
-        Ensure all logging output has been flushed.
+        """Ensure all logging output has been flushed.
 
         This version does nothing and is intended to be implemented by
         subclasses.
@@ -263,9 +258,7 @@ class Handler(object):
         pass
 
     def close(self):
-        """
-        Tidy up any resources used by the handler.
-        """
+        """Tidy up any resources used by the handler."""
         pass
 
     def handleError(self, record):
@@ -678,16 +671,6 @@ def basicConfig(**kwargs):
         if level is not None:
             root.setLevel(level)
 
-# Null handler
-
-class NullHandler(Handler):
-    """
-    This handler does nothing.
-    """
-    def emit(self, record):
-        pass
-
-# Warnings integration
 
 class log_warnings_to(object):
     """A context manager that copies and restores the warnings filter upon
