f1dbf42a87f33398f3e46d38dc290d08d889ab69
mitsuhiko <none@none>
2010-07-21 17:25:51 +0100

    Wrapped docstring

--- logbook.py
+++ logbook.py
@@ -530,19 +530,18 @@ class LoggerMixin(object):
 
 
 class Logger(LoggerMixin):
-    """Instances of the Logger class reself.level
-    present a single logging channel. A
-    "logging channel" indicates an area of an application. Exactly how an
-    "area" is defined is up to the application developer. Since an
+    """Instances of the Logger class reself.level present a single logging
+    channel. A "logging channel" indicates an area of an application. Exactly
+    how an "area" is defined is up to the application developer. Since an
     application can have any number of areas, logging channels are identified
-    by a unique string. Application areas can be nested (e.g. an area
-    of "input processing" might include sub-areas "read CSV files", "read
-    XLS files" and "read Gnumeric files"). To cater for this natural nesting,
-    channel names are organized into a namespace hierarchy where levels are
-    separated by periods, much like the Java or Python package namespace. So
-    in the instance given above, channel names might be "input" for the upper
-    level, and "input.csv", "input.xls" and "input.gnu" for the sub-levels.
-    There is no arbitrary limit to the depth of nesting.
+    by a unique string. Application areas can be nested (e.g. an area of "input
+    processing" might include sub-areas "read CSV files", "read XLS files" and
+    "read Gnumeric files"). To cater for this natural nesting, channel names
+    are organized into a namespace hierarchy where levels are separated by
+    periods, much like the Java or Python package namespace. So in the instance
+    given above, channel names might be "input" for the upper level, and
+    "input.csv", "input.xls" and "input.gnu" for the sub-levels.  There is no
+    arbitrary limit to the depth of nesting.
     """
 
     def __init__(self, name=None, level=NOTSET):
