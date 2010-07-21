9ca2291f49188b5ecd6df09fd5230e321eb91b2d
Georg Brandl <georg@python.org>
2010-07-21 15:36:47 +0100

    Add FileHandler tests.

--- test_logbook.py
+++ test_logbook.py
@@ -1,19 +1,64 @@
-import unittest
 import logbook
 
+import os
+import shutil
+import unittest
+import tempfile
 
-class BasicAPITestCase(unittest.TestCase):
+
+class LogbookTestCase(unittest.TestCase):
+    def setUp(self):
+        self.log = logbook.Logger('testlogger')
+
+
+class BasicAPITestCase(LogbookTestCase):
 
     def test_basic_logging(self):
-        logger = logbook.Logger('Test Logger')
         handler = logbook.TestHandler()
         with handler.contextbound(bubble=False):
-            logger.warn('This is a warning.  Nice hah?')
+            self.log.warn('This is a warning.  Nice hah?')
 
         assert handler.has_warning('This is a warning.  Nice hah?')
         assert handler.formatted_records == [
-            '[WARNING] Test Logger: This is a warning.  Nice hah?'
+            '[WARNING] testlogger: This is a warning.  Nice hah?'
         ]
+
+
+class HandlerTestCase(LogbookTestCase):
+
+    def setUp(self):
+        LogbookTestCase.setUp(self)
+        self.dirname = tempfile.mkdtemp()
+        self.filename = os.path.join(self.dirname, 'log.tmp')
+
+    def tearDown(self):
+        shutil.rmtree(self.dirname)
+        LogbookTestCase.tearDown(self)
+
+    def test_file_handler(self):
+        formatter = logbook.SimpleFormatter(
+            '{record.level_name}:{record.logger_name}:{record.message}')
+        handler = logbook.FileHandler(self.filename)
+        handler.formatter = formatter
+        with handler.contextbound():
+            self.log.warn('warning message')
+        handler.close()
+        with open(self.filename) as f:
+            self.assertEqual(f.readline(),
+                             'WARNING:testlogger:warning message\n')
+
+    def test_lazy_file_handler(self):
+        formatter = logbook.SimpleFormatter(
+            '{record.level_name}:{record.logger_name}:{record.message}')
+        handler = logbook.LazyFileHandler(self.filename)
+        handler.formatter = formatter
+        self.assertFalse(os.path.isfile(self.filename))
+        with handler.contextbound():
+            self.log.warn('warning message')
+        handler.close()
+        with open(self.filename) as f:
+            self.assertEqual(f.readline(),
+                             'WARNING:testlogger:warning message\n')
 
 
 if __name__ == '__main__':
