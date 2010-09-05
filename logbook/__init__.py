631cd54bb826e6f7ce5465f42b594af5de6ae4e9
Daniel Neuhäuser <dasdasich@gmail.com>
2010-09-05 12:53:20 +0200

    Expose exception and catch_exceptions as globals functions as documented

--- logbook/__init__.py
+++ logbook/__init__.py
@@ -29,6 +29,8 @@ warn = _default_logger.warn
 warning = _default_logger.warning
 notice = _default_logger.notice
 error = _default_logger.error
+exception = _default_logger.exception
+catch_exceptions = _default_logger.catch_exceptions
 critical = _default_logger.critical
 log = _default_logger.log
 del _default_logger
