a9805aebf0eae9a8c6fa778738b712a83fdc2b80
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2012-12-05 22:18:43 +0000

    Fixed MailHandler with unicode messages for Python 2.4 and 2.5

--- logbook/handlers.py
+++ logbook/handlers.py
@@ -1111,6 +1111,12 @@ class MailHandler(Handler, StringFormatterHandlerMixin,
         if suppressed:
             body += '\r\n\r\nThis message occurred additional %d ' \
                     'time(s) and was suppressed' % suppressed
+
+        # inconsistency in Python 2.4 and 2.5
+        # other versions correctly return msg.get_payload() as str
+        if sys.version_info < (2, 6) and isinstance(body, unicode):
+            body = body.encode('utf-8')
+
         msg.set_payload(body, 'UTF-8')
         return msg
 
