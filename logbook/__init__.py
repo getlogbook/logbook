c71f7b03171acff0bbbbacf8e11d4c01e1b25720
Rotem Yaari <vmalloc@gmail.com>
2015-08-04 17:55:54 +0300

    Bump version number to 0.10.2-dev

--- logbook/__init__.py
+++ logbook/__init__.py
@@ -23,7 +23,7 @@ from logbook.handlers import Handler, StreamHandler, FileHandler, \
      LimitingHandlerMixin, WrapperHandler, FingersCrossedHandler, \
      GroupHandler
 
-__version__ = '0.10.1'
+__version__ = '0.10.2-dev'
 
 # create an anonymous default logger and provide all important
 # methods of that logger as global functions
