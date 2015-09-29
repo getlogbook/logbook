b2abfeaf2e5460af530c4a00f5fde4832ed63cea
Rotem Yaari <vmalloc@gmail.com>
2015-09-29 17:03:22 +0300

    Bump version number to 0.11.2

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ def run_setup(with_binary):
         features['speedups'] = speedups
     setup(
         name='Logbook',
-        version='0.11.2-dev',
+        version='0.11.2',
         license='BSD',
         url='http://logbook.pocoo.org/',
         author='Armin Ronacher, Georg Brandl',
