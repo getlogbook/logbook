50bac555ba66203381e8d8ef909704e75d2be736
Frazer McLean <frazer@frazermclean.co.uk>
2015-10-17 20:16:42 +0200

    Add AppVeyor CI scripts.

---
+++ .appveyor/prepare.bat
@@ -0,0 +1,12 @@
+pip install wheel
+nuget install redis-64 -excludeversion
+redis-64\redis-server.exe --service-install
+redis-64\redis-server.exe --service-start
+nuget install ZeroMQ
+%WITH_COMPILER% pip install cython redis pyzmq
+python scripts\test_setup.py
+python setup.py develop
+IF DEFINED CYBUILD (
+	cython logbook\_speedups.pyx
+	%WITH_COMPILER% python setup.py build
+)
