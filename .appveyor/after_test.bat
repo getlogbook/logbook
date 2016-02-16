c60c14b494d941406e8a43fdddf00c3e8ae79797
Frazer McLean <frazer@frazermclean.co.uk>
2016-02-16 22:47:51 +0100

    Try latest config from python-packaging-user-guide

--- .appveyor/after_test.bat
+++ .appveyor/after_test.bat
@@ -1,5 +1,5 @@
 IF DEFINED CYBUILD (
-	%WITH_COMPILER% python setup.py bdist_wheel
+	%BUILD% python setup.py bdist_wheel
 	IF "%APPVEYOR_REPO_TAG%"=="true" (
 		twine upload -u %PYPI_USERNAME% -p %PYPI_PASSWORD% dist\*.whl
 	)
